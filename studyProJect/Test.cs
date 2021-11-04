fa4c137205353c43c8a55d8c2e24408a763d3e5e
id971003 <id971003@gmail.com>
2021-11-05 00:01:58 +0900

    day4 Add TestClass

--- studyProJect/Test.cs
+++ studyProJect/Test.cs
@@ -8,7 +8,7 @@ namespace projectstudy
     {
         static void Main(string[] arg)
         {
-            StackHeap.StackHeapTestClass();
+            //StackHeap.StackHeapTestClass();
         }
     }
 }
