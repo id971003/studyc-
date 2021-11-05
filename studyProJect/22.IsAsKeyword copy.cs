7ce83308fb0fb08af290cbf4eda508cd1f50351d
id971003 <id971003@gmail.com>
2021-11-05 14:13:55 +0900

    day 5

--- studyProJect/22.IsAsKeyword copy.cs
+++ studyProJect/22.IsAsKeyword copy.cs
@@ -8,26 +8,25 @@ namespace projectstudy
     파생클래스는 기반 클래스의 인스턴스로 이용가능하다 
     is : 객체가 해당형식에 해당하면 true 아니면 false 반환
     as : 형식 변환 자와같은 역할을 하지만 형식변환자는 형변환에 실패시 > 예외를 던지고 as 키워드는 실패시  > 객체 참조를 null 로 만듬 
+        + as 연산자는 참조 형식에만 가능하므로 형식변환자보다 as 연산자의 이용이 권장되지만 값형식일경우 형식 변환 연산자를 이용해야한다 
      */
     class IsAsKeyword
     {
         public static void IsAsKeywordTestClass()
         {
             Animal animal =new Dog(); //파생클래스를 기반클래스의 인스턴스로 이용
-
-
             Dog dog;
 
-            if(animal is Dog)
+            if(animal is Dog) //개가 동물이냐 ?  맞으니 true 
             {
-                dog = (Dog)animal;
+                dog = (Dog)animal; //animal 이 Dog 형식이니 형변환한다
                 dog.Bark();
             }
 
 
-            Animal animal2 = new Cat();
-            Cat cat1 = animal2 as Cat;
-            Cat cat2 = animal as Cat;
+            Animal animal2 = new Cat(); //
+            Cat cat1 = animal2 as Cat;  //고양이가 동물이냐 ? 맞으니 cat1 을 Cat 로 형변환
+            Cat cat2 = animal as Cat; //고양이가 개냐?  틀리니 cat2  을 null 로만든다
 
             if(cat1 !=null)
             {
---
+++ studyProJect/23.OverridingKeywold.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projectstudy
+{
+    /* 
+    오버라이딩 : 상속 과정에서 기반 클레스의 메서드에 변화를 주고 싶을 때 이용한다.
+    기반클레스에서 변화를 주고싶은 메서드를 virtual 로 한정하고 파생클레스에선 override 로 한정해이용한다
+        + private 선언시 상속 자체가 불가능함으로 오버라이딩은 불가능하다 
+    new : new 한정자로 메서드를 한정하면 Base 클레스에서의 메서드를 감추고 Derived 클레스의 메서드만 노출한다
+
+    오버라이딩vs new 를 이용한 메서드 숨기기 : 메서드 숨기기는 오버라이딩과 다르게 BaseClass 의 메서드도 실행가능하다 
+
+    sealed : 오버라이딩이 불가능하다 막는다  virtual 선언된 가상메소드를 오버라이딩 한 버전의 메소드만 가능 
+     */
+
+     
+    class OverridingKeywold
+    {
+        public static void OverridingKeywoldTestClass()
+        {
+            OverringDerivedClass OverD=new OverringDerivedClass();
+
+            OverD.Number(); //파생클래스의 number 메서드 
+
+            OverringBaceClass OverD2 = new OverringDerivedClass();
+
+            OverD2.Number2(); //기반클래스의 number 메서드 
+        }
+    }
+
+    class OverringBaceClass
+    {
+        public virtual void Number() // 상속시 변화를 주고싶은 메서드 
+        {
+            Console.WriteLine("1,2");
+        }
+        public virtual void Number2() // 상속시 변화를 주고싶은 메서드 
+        {
+            Console.WriteLine("5,6");
+        }
+    }
+    class OverringDerivedClass : OverringBaceClass
+    {
+        public sealed override void Number() //상속받은 메서드중에 변화를 줄 메서드  //sealed했기때문에 더이상 상속 불가능 
+        {
+            base.Number();
+            Console.WriteLine(",3,4,5");
+        }
+        public  new void Number2() // 상속시 변화를 주고싶은 메서드 
+        {
+            Console.WriteLine("7,8");
+        }
+    }
+}
