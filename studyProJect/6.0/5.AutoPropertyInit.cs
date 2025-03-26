fc7d5a93a41df9fdb3b20aeb1261d761675f545e
id971003 <id971003@gmail.com>
2025-03-26 19:22:37 +0900

    add AutoProperty , Expressmember

---
+++ studyProJect/6.0/5.AutoPropertyInit.cs
@@ -0,0 +1,24 @@
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace studyProJect
+{
+
+    public static class AutoPropertyInit
+    {
+        public static class User
+        {
+            //프로퍼티 init 하면서 자동으로 기본값 설정가능
+            public static string Name{get;set;} ="KIM";
+
+            //일기전용 프로퍼티
+
+            public static int Age{get;} =5;
+
+            
+        }
+    }
+}
---
+++ studyProJect/6.0/6.Expressmember.cs
@@ -0,0 +1,22 @@
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace studyProJect
+{
+    //속성이나 메서드 간단하게 표현가능
+    public static class ExpressMember
+    {
+        public static string FirstName;
+        public static string LastName;
+        
+
+        public static string BeforeName()
+        {
+            return FirstName+""+LastName;
+        }
+        public static string NewName=>FirstName+""+LastName;
+    }
+}
