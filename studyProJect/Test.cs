671583d375a98c06c32ab746eb8d96bedcfe6673
id971003 <id971003@gmail.com>
2025-03-27 19:04:59 +0900

    add 9.0 TODO

--- studyProJect/Test.cs
+++ studyProJect/Test.cs
@@ -13,20 +13,7 @@ namespace projectstudy
     {
         static void Main(string[] arg)
         {
-            Indexing ta = new Indexing();
-            ta.TA();
-
-        }
-        static async void AA()
-        {
-            Console.WriteLine("1");
-            await WaitForSecond(5);
-            Console.WriteLine("2");   
-        }
-        public static async Task WaitForSecond(int a)
-        {
-            await Task.Delay(1000); // 1�� ��� (�񵿱���)
-            Console.WriteLine("5");
+            
         }
 
     }
